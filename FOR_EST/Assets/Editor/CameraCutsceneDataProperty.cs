bfe1c5a86710dac614784f72bcdc99ebe12c0cbd
baewhv <baewhv@naver.com>
2026-04-06 11:24:45 +0900

    [FIX] : 캐릭터, 카메라 초기설정 중 펼치면 아래 항목에 개행이 들어가도록 수정.

--- FOR_EST/Assets/Editor/CameraCutsceneDataProperty.cs
+++ FOR_EST/Assets/Editor/CameraCutsceneDataProperty.cs
@@ -13,23 +13,24 @@ public class CameraCutsceneDataProperty : PropertyDrawer
         property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label);
         if (property.isExpanded)
         {
+            EditorGUI.indentLevel++;
             var target = property.FindPropertyRelative("followTarget");
             EditorGUILayout.PropertyField(target,
-                new GUIContent(" 타겟", "체크 시 지정된 캐릭터로 타겟팅합니다. 체크하지 않으면 좌표로 이동합니다."));
+                new GUIContent("타겟", "체크 시 지정된 캐릭터로 타겟팅합니다. 체크하지 않으면 좌표로 이동합니다."));
             if (target.boolValue)
             {
                 EditorGUILayout.PropertyField(property.FindPropertyRelative("target"),
-                    new GUIContent(" 타겟 캐릭터", "타겟팅할 캐릭터 목록입니다."));
+                    new GUIContent("타겟 캐릭터", "타겟팅할 캐릭터 목록입니다."));
             }
             else
             {
                 EditorGUILayout.PropertyField(property.FindPropertyRelative("position"),
-                    new GUIContent(" 위치", "해당 좌표에 배치합니다."));
+                    new GUIContent("위치", "해당 좌표에 배치합니다."));
             }
             EditorGUILayout.PropertyField(property.FindPropertyRelative("zoom"),
-                new GUIContent(" 줌", "줌 값을 설정합니다. 기본값 11.5, 작으면 줌인, 크면 줌아웃"));
+                new GUIContent("줌", "줌 값을 설정합니다. 기본값 11.5, 작으면 줌인, 크면 줌아웃"));
             
-            
+            EditorGUI.indentLevel--;
         }
 
         EditorGUI.EndProperty();
